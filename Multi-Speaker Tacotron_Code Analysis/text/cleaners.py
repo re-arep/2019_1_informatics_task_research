cdb3f698d5593896aa08d74529eb6a1828bbdc6b
J_daeum <37567626+Jdaeum@users.noreply.github.com>
2019-02-14 23:22:24 +0900

    Update cleaners.py

--- Multi-Speaker Tacotron_Code Analysis/text/cleaners.py
+++ Multi-Speaker Tacotron_Code Analysis/text/cleaners.py
@@ -9,6 +9,15 @@ hyperparameter. Some cleaners are English-specific. You'll typically want to use
          the Unidecode library (https://pypi.python.org/pypi/Unidecode)
     3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
          the symbols in symbols.py to match your data).
+
+클리너는 교육 및 평가시 입력 텍스트를 통해 실행되는 변환입니다.
+클리너는 쉼표로 구분 된 클리너 이름 목록을 "클리너"로 전달하여 선택할 수 있습니다.
+하이퍼 파라미터. 일부 클리너는 영어에 따라 다릅니다. 일반적으로 다음을 사용하려고합니다.
+    1. 영어 텍스트의 경우 "english_cleaners"
+    2. ASCII를 사용하여 ASCII로 음역 될 수있는 영어가 아닌 텍스트의 경우 "transliteration_cleaners"
+         Unidecode 라이브러리 (https://pypi.python.org/pypi/Unidecode)
+    3. 음역을 사용하지 않으려면 "basic_cleaners"를 입력하십시오 (이 경우,
+         귀하의 데이터와 일치하는 symbols.py의 기호)
 '''
 
 import re
